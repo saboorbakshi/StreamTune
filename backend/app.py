7f1f39f923c9086d4cf0c715383583178eb9851b
Kevin LIu <kevin.liu@lifecacheapp.com>
2024-02-16 01:39:38 -0500

    setup Flask and integrated Firebase

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,16 @@
-from flask import Flask
+#Imports
+from flask import Flask, request, jsonify
+import firebase_admin
+from firebase_admin import db, credentials
+
+app = Flask(__name__)
+
+cred = credentials.Certificate('credentials.json')
+firebase_admin.initialize_app(cred)
+
+@app.route("/")
+def home():
+    return "Demo root"
+
+if __name__ == "__main__":
+    app.run(debug=True)
