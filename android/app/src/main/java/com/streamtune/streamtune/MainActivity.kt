6a3ab6abc83e1e4a213cf0a3e5d0ae09c745c296
Saboor Bakshi <saboor@Saboors-MacBook.local>
2024-02-16 04:35:21 -0500

    login screen

--- android/app/src/main/java/com/streamtune/streamtune/MainActivity.kt
+++ android/app/src/main/java/com/streamtune/streamtune/MainActivity.kt
@@ -3,13 +3,38 @@ package com.streamtune.streamtune
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.streamtune.streamtune.ui.theme.StreamTuneTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +43,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             StreamTuneTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
+                    Greeting("LOGIN")
                 }
             }
         }
@@ -27,11 +52,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun Greeting(title: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "LOGIN",
+            style = MaterialTheme.typography.bodyLarge.copy(fontSize = 30.sp,
+                                                            fontWeight = FontWeight.Bold)
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        var username by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("username") }
+        )
+        var password by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("password") }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(
+            onClick = { /* Handle login button click */ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black
+            ),
+            modifier = Modifier
+                .width(150.dp)
+        ) {
+            Text("Login")
+        }
+        Button(
+            onClick = { /* Handle login button click */ },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black
+            ),
+            modifier = Modifier
+                .width(150.dp)
+
+        ) {
+            Text("Register")
+        }
+    }
 }
 
 @Preview(showBackground = true)
